bcbb79a788afefe386222ee6bc33582242e3296c
Nicholas N <nnlaor@gmail.com>
2017-04-23 23:44:06 -0700

    adding npm run devstart for nodemon

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ApartmentSite
 Dynamic property management app
+
+- npm install
+
+- npm run devstart
+
+- visit 
+
+- rs to restart server manually, but it should be restarting on file change locally
+
+- or npm start
--- app.js
+++ app.js
@@ -1,20 +1,25 @@
+// this file creates the express application
+
 var express = require('express');
-
-
+var path = require('path');
+var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 
 
 
 // create the app object
+// this object has key value pairs some of which we will be setting
 var app = express();
 
 // view engine setup
+// set 'views' value to specify the folder where templates will be stored
 app.set('views', path.join(__dirname, 'views'));
+// set the 'view engine' value to 'pug'
 app.set('view engine', 'pug');
 
 
-// middle ware
+// app.use does middleware
 app.use(bodyParser.json());
 
-
+// this line allows the express object to be imported by /bin/www
 module.exports = app;
