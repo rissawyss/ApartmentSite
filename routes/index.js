74e49eb71cb20e7964e241823a13e19972c0120b
Nicholas Noochla-or <nnlaor@gmail.com>
2017-05-10 17:52:32 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = requrire('../models');
+var db = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
