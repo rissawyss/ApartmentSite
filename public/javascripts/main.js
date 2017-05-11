d17ad21ad1090325ba6c227585e28f2f7e67baaf
Bryan Castleman <Bryan Castleman>
2017-05-10 20:43:43 -0700

    adding changes to main.js and index.html in order to capture data from contact form

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,35 @@
 $(document).ready(function() {
 
+    // Initialize the Select Element
     $('select').material_select();
+    // Date Picker
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 2 // Creates a dropdown of 2 years to control year
     });
+    // Side Navigation collapse 
     $(".button-collapse").sideNav();
     console.log("ready!");
 
-    var firstName = $("#author-name");
-    var lastName = $("#last_name");
-    var email = $("#email");
-    var phone = $('#icon_telephone');
-    var rooms = $('#rooms');
-    var moveIn = $('#Move-in');
-    var textarea1 = $('#textarea1');
+    // Prospect Form
+    var firstName = $("#first_name").val().trim();
+    var lastName = $("#last_name").val().trim();
+    var email = $("#email").val().trim();
+    var phone = $('#telephone').val();
+    var rooms = $('#bedrooms' :checked).val(); // testing this jquery to get checked value 
+    var moveIn = $('#move-in').val();
+    var textarea1 = $('#textarea1').val().trim();
 
 
+    // Event listener for Submit Button on Contact Form 
+    $("form").submit(function(){
+        console.log("submitted");
+        handleContactSubmit();
+    });
+    
 
-    $(document).on("submit", "#contact", handleContactSubmit);
+
+    // $("#contactForm").on("click", "#contactForm", handleContactSubmit);
 
 
     function handleContactSubmit(event) {
@@ -29,18 +40,21 @@ $(document).ready(function() {
             return;
         }
 
-        postApplicant({
-            firstname: firstName.val.trim(),
-            lastname: lastName.val.trim()
-
-
+        postProspect({
+            firstname: firstName,
+            lastname: lastName,
+            email: email,
+            phone: phone,
+            rooms: rooms,
+            moveIn: moveIn,
+            textarea1: textarea1
         });
     }
 
 
 
-    function postApplicant(userData){
-    	$.post("/applicants", userData)
+    function postProspect(userData){
+    	$.post("/prospects", userData)
     	.then(console.log('thank you for submiting'));
     }
 
