1ebf8caafe31196ba1a00232e25b11fbb6a8365b
Nicholas N <nnlaor@gmail.com>
2017-05-16 13:27:47 -0700

    removed signup from resident

--- controllers/residentController.js
+++ controllers/residentController.js
@@ -32,24 +32,3 @@ exports.workorder_delete_post = function(req, res, next){
   });
 };
 
-exports.signup = function(req, res, next){
-  db.User.findAll({
-    where: {username: req.body.username}
-  }).then(function(users){
-    if(users.length > 0){
-      res.json({
-        duplicateUser: true
-      });
-    }else {
-      db.User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-      }).then(function(){
-        res.send({redirect: '/'});
-      }).catch(function(err){
-        res.json(err);
-      });
-    }
-  });
-};
