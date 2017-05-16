168947c7e4e66224e431dbf48412fa4cae09a71d
Nicholas Noochla-or <nnlaor@gmail.com>
2017-05-16 16:02:02 -0700

    Update manager.js

--- routes/manager.js
+++ routes/manager.js
@@ -46,9 +46,9 @@ router.get('/tenants/:date?', function(req, res, next) {
         db.Tenant.findAll({
             limit: 10,
             order: [['lease_start', 'DESC']]
-        }).then(function(GoldieHawn){
+        }).then(function(tenantsData){
             // res.render('manager', {GoldieHawn});
-            res.json(GoldieHawn);
+            res.json(tenantsData);
         });
     }
     console.log('hit tenant');
