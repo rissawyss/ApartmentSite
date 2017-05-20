cba6ed4a659011d0ed00acb399644d5dfd549397
Nicholas N <nnlaor@gmail.com>
2017-05-20 01:47:58 -0700

    adding authentication

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
+  console.log('I am here, the login.js got loaded');
   // Getting references to our form and inputs
   var loginForm = $("form.login");
   var usernameInput = $("input#username-input");
   var passwordInput = $("input#password-input");
 
   // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", function(event) {
+  loginForm.on("click", function(event) {
     event.preventDefault();
     var userData = {
       username: usernameInput.val().trim(),
@@ -21,6 +22,7 @@ $(document).ready(function() {
     usernameInput.val("");
     passwordInput.val("");
   });
+  console.log('2 I am here, the login.js got loaded');
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
   function loginUser(username, password) {
@@ -28,11 +30,16 @@ $(document).ready(function() {
       username: username,
       password: password
     }).then(function(data) {
+      console.log(data);
       window.location.replace(data);
       // If there's an error, log the error
-    }).catch(function(err) {
-      console.log(err);
-    });
+    });//.catch(function(err) {
+    //   console.log(err);
+    // });
+    console.log(username + password);
   }
+
+
+  console.log('3 I am here, the login.js got loaded');
 
 });
